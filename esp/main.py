366d0f06acd4d074655c349019f253f2f2c7099c
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-10-02 15:18:00 -0600

    cleaned code + var names

--- esp/main.py
+++ esp/main.py
@@ -4,7 +4,7 @@ import json
 from sensor import read_sensor
 from simple import MQTTClient
 
-def load_config():
+def load_mqtt_config():
     try:
         with open('mqtt_config.json', 'r') as file:
             return json.load(file)
@@ -12,21 +12,21 @@ def load_config():
         print(f"Could not read MQTT config: {e}")
         return {}
 
-config = load_config()
+config = load_mqtt_config()
 
-MQTT_BROKER = config.get("MQTT_BROKER", "default_broker")
-MQTT_PORT = config.get("MQTT_PORT", 1883)
-SENSOR_TOPIC = config.get("SENSOR_TOPIC", "default_topic")
-DEVICE_CHANNEL= config.get("DEVICE_CHANNEL", "default")
+BROKER_ADDRESS = config.get("BROKER_ADDRESS", "default_broker")
+BROKER_PORT = config.get("BROKER_PORT", 1883)
+SENSOR_DATA_TOPIC = config.get("SENSOR_DATA_TOPIC", "default_topic")
+DISCOVERY_CHANNEL = config.get("DISCOVERY_CHANNEL", "default")
 
-CLIENT_ID = "water_sensor"
+DEVICE_ID = "water_sensor"
 
 def mqtt_callback(topic, msg):
     print(f"Received message: {msg} on topic: {topic}")
 
-def connect_mqtt():
+def connect_to_broker():
     try:
-        client = MQTTClient(CLIENT_ID, MQTT_BROKER, port=MQTT_PORT)
+        client = MQTTClient(DEVICE_ID, BROKER_ADDRESS, port=BROKER_PORT)
         client.set_callback(mqtt_callback)
         client.connect()
         return client
@@ -35,21 +35,20 @@ def connect_mqtt():
         return None
 
 def main():
-    client = connect_mqtt()
+    client = connect_to_broker()
     if client:
-        
         # Send initial sensor info
-        sensor_info = {
-            "device": CLIENT_ID,
-            "mqtt_topic": SENSOR_TOPIC,
+        initial_payload = {
+            "device_id": DEVICE_ID,
+            "data_topic": SENSOR_DATA_TOPIC,
         }
-        client.publish(DEVICE_CHANNEL, json.dumps(sensor_info))
+        client.publish(DISCOVERY_CHANNEL, json.dumps(initial_payload))
         
         while True:
             try:
                 sensor_value = read_sensor()
                 print(f"DEBUG: Water sensor value: {sensor_value}")
-                client.publish(SENSOR_TOPIC, str(sensor_value))
+                client.publish(SENSOR_DATA_TOPIC, str(sensor_value))
                 client.wait_msg()
                 time.sleep(1)
             except KeyboardInterrupt:
@@ -57,7 +56,7 @@ def main():
                 break
             except:
                 print("DEBUG: An error occurred. Trying to reconnect.")
-                client = connect_mqtt()
+                client = connect_to_broker()
                 time.sleep(5)
 
 if __name__ == "__main__":
